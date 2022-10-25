b2e5edd41317a3db626e1d167102ee390db8b6f4
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2022-10-25 21:37:06 +0700

    Create To Do List

---
+++ app.js
@@ -0,0 +1,52 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems=[];
+
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+
+  var today = new Date();
+  var options ={
+    weekday: "long",
+    day:"numeric",
+    month:"long"
+  };
+
+  var day = today.toLocaleDateString("en-Us", options);
+
+  res.render("list", {
+    listTitle:day, newListItems:items
+  });
+
+});
+
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.get("/work", function(res,req){
+  res.render("list", {
+    listTitle:"Work List", newListItems: workItems
+  });
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItem.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000,function(){
+  console.log("Server connect");
+});
